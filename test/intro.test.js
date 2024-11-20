e840f3bc37d5423e213f7613241b3759d29264ce
nemanja <antonijevicnemanja68@gmail.com>
2024-11-20 13:16:06 +0100

    second testing example

--- test/intro.test.js
+++ test/intro.test.js
@@ -1,8 +1,20 @@
 import { describe, test, it, expect} from "vitest"
-import { max } from "../src/intro"
+import { fizzBuzz, max } from "../src/intro"
 
 describe('max', () => {
     it('function max should return the first argument if it is greater', () => {
         expect(max(2, 1)).toBe(2);
+    })
+})
+
+describe('fizzBuzz', () => {
+    it('if number are % === 3 && number % === 5 ? should return FizzBuzz', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+    it('if number are % === 3 ? should return Fizz', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    })
+    it('if number are % === 5 ? should return Buzz', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
     })
 })
